4291937b7e4000a282a35121ff72b25269f1294e
Dlnvta <novitadela97@gmail.com>
2020-09-08 10:24:12 -0700

    selasa commit

---
+++ views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>XII RPL 2</title>
+		<style type="text/css">
+		</style>
+		
+	</head>
+	<body>
+  </hgroup>
+  <h1> About XII RPL 2</h1>
+   <p>XII RPL 2 adalah sebuah kelas dari salah satu jurusan di SMKN 1 Subang, yaitu jurusan Rekayasa Perangkat Lunak.
+   apa itu Rekayasa Perangkat Lunak? Saya tidak akan membahasnya, karena yang akan saya bahas itu kelas XII RPL 2
+   bukan jurusan Rekayasa perangkat Lunak. Kelas XII RPL 2 ini terdiri dari 35 murid, yakni 28 orang murid laki-laki dan 9 orang murid perempuan. Ya, murid
+   laki-laki lebih mayoritas daripada murid perempuan. Kenapa? entahlah saya tidak bisa menjelaskannya, terlalu rumit 
+   untuk dijelaskan teman-teman. Lanjut, kelas ini mempunyai wali kelas yang sangat luar biasa yaitu bapak Drs.Nur Nurasa
+   yang tak lain adalah guru mata pelajaran Bahasa Indonesia. Kelas ini juga mempunyai Ketua Murid yang juga sangat luar biasa,
+   adalah Dana Supriatna yang sudah 3 tahun berturut-turut menjabat sebagai Ketua Murid, ya, sudah dari kelas X beliau menjabat.
+   Tepuk tangan teman-teman. Apalagi ya? Sudah dulu ya nanti saya lanjutkan lagi. Ini hanya tulisan iseng, nanti saya buat
+   yang lebih serius untuk teman-teman sekalian. Byebyeeeee....
+
+
+	</body>
+</html>
---
+++ views/structure.blade.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+<title>Struktur</title>
+<body>
+<h1> Struktur Kelas</h1>
+    <table border="1">
+    
+    <tr align="center">
+    <th width="50px">No</th>
+    <th width="150px">Nama Lengkap</th>
+    <th width="100px">Jabatan</th>
+    </tr>
+ 
+    
+    <tr>
+    <td align="center">1</td>
+    <td>Dana Supriatna</td>
+    <td>Ketua Murid</td>
+    </tr>
+
+    <tr>
+    <td align="center">2</td>
+    <td>Amelia</td>
+    <td>Wakil Ketua Murid</td>
+    </tr>
+
+    <tr>
+    <td align="center">3</td>
+    <td>Riani Marcelina</td>
+    <td>Sekertaris 1</td>
+    </tr>
+
+    <tr>
+    <td align="center">4</td>
+    <td>Kharisma Aulia</td>
+    <td>Sekertaris 2</td>
+    </tr>
+
+    <tr>
+    <td align="center">5</td>
+    <td>Futri Kurnia Lestari</td>
+    <td>Bendahara 1</td>
+    </tr>
+
+    <tr>
+    <td align="center">6</td>
+    <td>Tika Amelia</td>
+    <td>Bendahara 2</td>
+    </tr>
+
+    <tr>
+    <td align="center">7</td>
+    <td>Rifky Aswangga</td>
+    <td>Seksi Perlengkapan</td>
+    </tr>
+
+    <tr>
+    <td align="center">8</td>
+    <td>Zulfikar Augusta</td>
+    <td>Seksi Kebersihan</td>
+    </tr>
+
+    <tr>
+    <td align="center">9</td>
+    <td>Isfahan Caturnanda</td>
+    <td>Seksi Perpustakaan</td>
+    </tr>
+
+    <tr>
+    <td align="center">10</td>
+    <td>Naufal Darmawan</td>
+    <td>Seksi Kesehatan</td>
+    </tr>
+
+    </table>
+
+</body>
+</head>
+</html>
